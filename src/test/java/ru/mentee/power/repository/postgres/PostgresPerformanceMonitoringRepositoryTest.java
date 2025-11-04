5e5710115706e7ab3c611cf1be0a477e5e20e4f6
pavelKomiss121 <pavelkomiss@gmail.com>
2025-11-04 17:54:06 +0300

    mp-165 - отключение теста

--- src/test/java/ru/mentee/power/repository/postgres/PostgresPerformanceMonitoringRepositoryTest.java
+++ src/test/java/ru/mentee/power/repository/postgres/PostgresPerformanceMonitoringRepositoryTest.java
@@ -17,6 +17,7 @@ import liquibase.database.DatabaseFactory;
 import liquibase.database.jvm.JdbcConnection;
 import liquibase.resource.ClassLoaderResourceAccessor;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.PostgreSQLContainer;
@@ -28,6 +29,7 @@ import ru.mentee.power.exception.DataAccessException;
 import ru.mentee.power.exception.SASTException;
 import ru.mentee.power.model.mp164.*;
 
+@Disabled("Only Liquibase tests should run")
 @Testcontainers
 public class PostgresPerformanceMonitoringRepositoryTest {
 
